45ca5d56136b1eb824ba6d367e1cc99ba1fa6dc7
azadtom <kumarazad2917@gmail.com>
2026-08-16 15:25:17 +0530

    refactor: remove admin panel module, controller, service, and DTOs

--- src/admin-panel/dto/update-admin-panel.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateAdminPanelDto } from './create-admin-panel.dto';
-
-export class UpdateAdminPanelDto extends PartialType(CreateAdminPanelDto) {}
