1a36b375793e4238ce792658ca46874e0bb18e1a
azadtom <kumarazad2917@gmail.com>
2026-08-16 19:47:47 +0530

    fix: remove DatabaseModule import from UserModule

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entities';
-import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
-  imports:[DatabaseModule, TypeOrmModule.forFeature([User],'authConnection')],
+  imports:[TypeOrmModule.forFeature([User],'authConnection')],
   providers: [UserService],
   exports:[UserService],
 })
