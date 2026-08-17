bdf60cd7a6f5b646541d76ca1c9c55197bd452d5
azadtom <kumarazad2917@gmail.com>
2026-08-17 15:26:48 +0530

    feat: set up view engine and base views directory for rendering

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,15 @@ import {AppModule} from './app.module';
 import {NestExpressApplication} from '@nestjs/platform-express';
 import cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use(cookieParser());
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
+  app.setBaseViewsDir(join(process.cwd(), 'views'));
+  app.setViewEngine('hbs');
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
